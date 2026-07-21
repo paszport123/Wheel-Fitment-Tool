fcdae5128c510a12fe80811cf65a65bd9a441106
Pawel <paulo.paszko@gmail.com>
2026-07-21 21:45:44 +0200

    talaria komodo front fork changes

--- update_talaria_komodo.py
+++ update_talaria_komodo.py
@@ -1,19 +1,19 @@
-import sqlite3
+# import sqlite3
 
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-
-
-cursor.execute("""
-UPDATE motorcycle
-SET sprocket_surface_to_swingarm_mm = -9
-WHERE brand = ? AND model = ?
-""", ("Talaria", "Komodo"))
+# conn = sqlite3.connect("database.db")
+# cursor = conn.cursor()
 
 
+# cursor.execute("""
+# UPDATE motorcycle
+# SET sprocket_surface_to_swingarm_mm = -9
+# WHERE brand = ? AND model = ?
+# """, ("Talaria", "Komodo"))
 
 
-conn.commit()
-conn.close()
 
-print("success.")
+
+# conn.commit()
+# conn.close()
+
+# print("success.")
