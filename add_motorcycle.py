7fbc9d294d57e382b60f650aee49b9ddffe06724
Pawel <paulo.paszko@gmail.com>
2026-07-27 13:36:46 +0200

    reorganised add motorcycle function

--- add_motorcycle.py
+++ add_motorcycle.py
@@ -4,15 +4,22 @@ import sqlite3
 brand = input("Motorcycle brand: ")
 model = input("Motorcycle model: ")
 
-rear_swingarm_width = float(input("Rear swingarm width [mm]: "))
-sprocket_surface_to_swingarm = float(input("Sprocket surface to swingarm [mm]: "))
-sprocket_adapter_offset_mm = float(input("sprocket adapter offset [mm]: "))
+# ---REAR WHEEL---
+rear_swingarm_width_mm = float(input("Rear swingarm width [mm]: "))
+sprocket_surface_to_swingarm_mm = float(input("Sprocket surface to swingarm [mm]: "))
+sprocket_adapter_offset_mm = float(input("Sprocket adapter offset [mm]: "))
+chain_overhang_mm = float(input("Chain overhang [mm]: "))
+# minimum_chain_clearance_mm = float(input("minimum chain clearance (5mm + ((chain width - sprocket width) / 2) [mm]"))
 
-front_fork_width_mm = float(input("Front fork width at axle [mm]: "))
-front_brake_surface_to_fork_mm = float(input("Front brake surface to fork [mm]: "))
-minimum_chain_clearance_mm = float(input("minimum chain clearance (5mm + ((chain width - sprocket width) / 2) [mm]"))
+# ---FRONT WHEEL---
+front_dish_required = int(input("Is the front dish required? (1-Yes, 0-No)"))
+
+if front_dish_required is 1:
+    front_fork_width_mm = float(input("Front fork width at axle [mm]: "))
+    front_brake_surface_to_fork_mm = float(input("Front brake surface to fork [mm]: "))
 
 notes = input("Notes: ")
+
 
 # Connect to database
 conn = sqlite3.connect("database.db")
@@ -25,22 +32,27 @@ INSERT INTO motorcycle (
     model,
     rear_swingarm_width_mm,
     sprocket_surface_to_swingarm_mm,
+    notes,
     sprocket_adapter_offset_mm,
     front_fork_width_mm,
     front_brake_surface_to_fork_mm,
+    front_dish_required,
     minimum_chain_clearance_mm,
-    notes
+    chain_overhang_mm
 )
-VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 """, (
     brand,
     model,
-    rear_swingarm_width,
-    sprocket_surface_to_swingarm,
+    rear_swingarm_width_mm,
+    sprocket_surface_to_swingarm_mm,
+    notes,
     sprocket_adapter_offset_mm,
     front_fork_width_mm,
     front_brake_surface_to_fork_mm,
-    notes
+    front_dish_required,
+    # minimum_chain_clearance_mm,
+    chain_overhang_mm
 ))
 
 # Save changes
